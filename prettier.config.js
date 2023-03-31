b43ad093db539baac1a85cf6d8adbc72d38f8f0c
Christopher Pezza <chris.pezza@hey.com>
2023-03-31 10:20:30 -0400

    formatting update for repo files

--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,7 @@
 module.exports = {
-	plugins: [require('prettier-plugin-tailwindcss')],
-	semi: false,
-	singleQuote: false,
-	printWidth: 100,
-	trailingComma: 'es5',
-	arrowParens: 'always',
-	tabWidth: 2,
-	useTabs: false,
-	quoteProps: 'as-needed',
-	jsxSingleQuote: false,
-	bracketSpacing: true,
+  plugins: [require("prettier-plugin-tailwindcss")],
+  singleQuote: false,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
 };
